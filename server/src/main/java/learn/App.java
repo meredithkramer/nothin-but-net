e4e42df500bce3b9b96319a237bd6426e382cfb2
BHL1994 <brien.hewanlowe@gmail.com>
2024-08-08 09:30:15 -0400

    added App class

---
+++ server/src/main/java/learn/App.java
@@ -0,0 +1,19 @@
+package learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
