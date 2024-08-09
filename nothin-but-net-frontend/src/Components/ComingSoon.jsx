c1032d77226073de70c536ade5dcb969c423610a
mquail1 <quailmeredith@gmail.com>
2024-08-09 09:52:20 -0700

    implementing / styling comingsoon cmpt

--- nothin-but-net-frontend/src/Components/ComingSoon.jsx
+++ nothin-but-net-frontend/src/Components/ComingSoon.jsx
@@ -1,7 +1,13 @@
 export default function ComingSoon() {
   return (
-    <>
-      <h1>Coming Soon...</h1>
-    </>
+    <div className="coming-soon">
+      {/* <h1 className="cs-header">Coming Soon</h1> */}
+      <h1 className="cs-header" id="cs-sub-header">
+        This feature is currently on our roadmap
+      </h1>
+      <div className="cs-header-underline"></div>
+      <span>Questions, comments, or concerns? Feel free to </span>
+      <span className="cs-fake-email">reach out to us</span>
+    </div>
   );
 }
